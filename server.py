9cfbe8c1f31526c750e919426f66413296a3f6b4
Andre Schickhoff <achaville@hotmail.de>
2022-07-11 11:57:34 +0200

    fixed booking more places than available

--- server.py
+++ server.py
@@ -52,9 +52,19 @@ def purchasePlaces():
     ]
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
     placesRequired = int(request.form["places"])
-    competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
-    flash("Great-booking complete!")
-    return render_template("welcome.html", club=club, competitions=competitions)
+
+    if placesRequired > int(competition["numberOfPlaces"]):
+        flash("There are not enough places available.")
+        return (
+            render_template("welcome.html", club=club, competitions=competitions),
+            403,
+        )
+    else:
+        competition["numberOfPlaces"] = (
+            int(competition["numberOfPlaces"]) - placesRequired
+        )
+        flash("Great-booking complete!")
+        return render_template("welcome.html", club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
