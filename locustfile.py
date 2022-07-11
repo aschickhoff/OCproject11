f533228458b574455e1eef6b656bd0f49ee6e509
Andre Schickhoff <achaville@hotmail.de>
2022-07-11 09:10:28 +0200

    added locustfile

---
+++ locustfile.py
@@ -0,0 +1,42 @@
+from locust import HttpUser, task, between
+from server import loadClubs, loadCompetitions
+
+
+class LocustServerClient(HttpUser):
+    wait_time = between(1, 5)
+    club = loadClubs()[3]
+    competition = loadCompetitions()[3]
+
+    def on_start(self):
+        self.client.get("/")
+
+    @task
+    def loadClubs(self):
+        self.club = loadClubs()[3]
+
+    @task
+    def loadCompetitions(self):
+        self.competition = loadCompetitions()[3]
+
+    @task
+    def showSummary(self):
+        self.client.post("/showSummary", data={"email": self.club["email"]})
+
+    @task()
+    def book(self):
+        self.client.get(f"/book/{self.competition['name']}/{self.club['name']}")
+
+    @task
+    def purchasePlaces(self):
+        self.client.post(
+            "/purchasePlaces",
+            data={
+                "places": 1,
+                "club": self.club["name"],
+                "competition": self.competition["name"],
+            },
+        )
+
+    @task
+    def logout(self):
+        self.client.get("/logout")
